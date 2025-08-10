2c2adcb0cb88b2c0ad6e3c0736daf8e3a10bbe1f
PhamVanTea <23633471.tra@student.iuh.edu.vn>
2025-08-10 20:10:16 +0700

    feat: validate form for register

---
+++ shoppe-clone/src/utils/rules.ts
@@ -0,0 +1,62 @@
+import { RegisterOptions } from 'react-hook-form';
+
+// type Rules = {
+//   [key in 'email' | 'password' | 'confirm_password']?: RegisterOptions;
+// };
+interface FormData {
+  email: string;
+  password: string;
+  confirm_password: string;
+}
+type Rules = {
+  [key in keyof FormData]?: RegisterOptions<FormData, key>;
+};
+
+export const rules: Rules = {
+  email: {
+    required: {
+      value: true,
+      message: 'Email không được để trống !',
+    },
+    pattern: {
+      value: /^\S+@\S+\.\S+$/,
+      message: 'Email nhập không đúng định dạng !',
+    },
+    maxLength: {
+      value: 160,
+      message: 'Độ dài từ 5-160 ký tự',
+    },
+    minLength: {
+      value: 5,
+      message: 'Độ dài từ 5-160 ký tự',
+    },
+  },
+  password: {
+    required: {
+      value: true,
+      message: 'password không được để trống !',
+    },
+    maxLength: {
+      value: 160,
+      message: 'Độ dài từ 6-160 ký tự',
+    },
+    minLength: {
+      value: 6,
+      message: 'Độ dài từ 6-160 ký tự',
+    },
+  },
+  confirm_password: {
+    required: {
+      value: true,
+      message: 'Confirm password không được để trống !',
+    },
+    maxLength: {
+      value: 160,
+      message: 'Độ dài từ 6-160 ký tự',
+    },
+    minLength: {
+      value: 6,
+      message: 'Độ dài từ 6-160 ký tự',
+    },
+  },
+};
